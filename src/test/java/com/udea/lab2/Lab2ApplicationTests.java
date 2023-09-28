403ee026808005d2df13ae5c86060d5f7533e9a2
Juan Fernando Lopera Muñoz <100541364+JL005@users.noreply.github.com>
2023-09-28 17:25:48 -0500

    Inicio del proyecto

---
+++ src/test/java/com/udea/lab2/Lab2ApplicationTests.java
@@ -0,0 +1,36 @@
+package com.udea.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class Lab2ApplicationTests {
+
+	@Autowired
+	DataController dataController;
+
+	@Test
+	void health() {
+		assertEquals("Se murió la gonorrea", dataController.healthCheck());
+	}
+
+	@Test
+	void version() {
+		assertEquals("Versión 1.0", dataController.version());
+	}
+
+	@Test
+	void nationLength() {
+		Integer nationsLength = dataController.getRandomNations().size();
+		assertEquals(10, nationsLength);
+	}
+
+	@Test
+	void currenciesLength() {
+		Integer nationsLength = dataController.getRandomCurrencies().size();
+		assertEquals(20, nationsLength);
+	}
+}
